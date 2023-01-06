2d124629e193e5c35db338ccc360dc034c945af0
moons <rainstorm@gmail.com>
2023-01-07 08:48:39 +0900

    almost

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import threading
 import os
 import re
 import string
+import sys
 from urllib.parse import urlparse
 from collections import namedtuple
 
@@ -30,6 +31,25 @@ class Fuzz:
         self.wordListArray = args.wordlist.split(',')
         self.wordListFile = args.wordlist.split(',')
         
+        # set output file
+        if self.args.remoteName != '' or self.args.outfile == True:
+            if self.args.outfile == True:
+                # file  already existed
+                if os.path.exists('fuzz.txt'):
+                    with open('fuzz.txt', 'r') as f:
+                        for line in f.readlines():
+                            if line.strip() != '':
+                                print('"fuzz.txt" File already existed')
+                                sys.exit()
+                fname = 'fuzz.txt'
+            else:
+                fname = self.args.remoteName
+
+            self.file = open(file=fname, mode='w')
+        else:
+            self.file = None
+
+        # manage threads
         self.threads = list()
         for i in range(len(self.signlist)):
             
@@ -59,6 +79,20 @@ class Fuzz:
         for i in range(len(self.signlist)):
             print(f'{self.signlist[i].file}<-{self.signlist[i].sign}')
 
+    def exit(self, e=''):
+        print(e)
+        self.stdout('\n' + '-' * 30 + '{Missing Pachket}' + '-' * 30 + '\n')
+        for line in self.missingPacket:
+            self.stdout(line)
+        if self.file != None:
+            self.file.close()
+
+    def stdout(self, string, end='\n'):
+        if self.file == None:
+            print(string, end=end)
+        else:
+            self.file.write(string)
+
     def run(self):
         print(self.args.header)
         mark = ['' for i in range(len(self.signlist))]
@@ -72,13 +106,20 @@ class Fuzz:
                 break
             payload = self.args.url
             for i in range(len(self.signlist)): # here 
+                
+                # add rule
                 mark[i] = eval(self.signlist[i].exp.replace('$', 'mark[i]'))
                 payload = payload.replace(self.signlist[i].sign, str(mark[i])).strip()
+                self.args.data = self.args.data.replace(self.signlist[i].sign, str(mark[i]))
+                self.args.header = self.args.header.replace(self.signlist[i].sign, str(mark[i]))
+                self.args.timeout = self.args.timeout.replace(self.signlist[i].sign, str(mark[i]))
 
             attack_thread = threading.Thread(target=self.attack, args=(payload,))
             attack_thread.daemon = True # daemon thread
             attack_thread.start()
             self.threads.append(attack_thread)
+            if threading.active_count() > self.args.thread:
+                attack_thread.join()
 
     # multi threading
     def attack(self, payload):
@@ -101,7 +142,7 @@ class Fuzz:
                 result += 'X]'
         if result != '':
             result = f'{payload:45s}' + result
-            print(f'{result}\n', end='') # end = '\n' then it have multi thraeding problem
+            self.stdout(f'{result}\n', end='') # end = '\n' then it have multi thraeding problem
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -113,6 +154,8 @@ if __name__ == '__main__':
         )
 
     parser.add_argument('-w', '--wordlist', help='word list', default="digits.txt:{0}:,digits1.txt:{1}")
+    parser.add_argument('-o', '--outfile', action='store_true', help='Wirte output to a file name as the remoted name')
+    parser.add_argument('-O', '--remoteName', help='Write to file instead of stdout', default='')
 
     # http options
     parser.add_argument('-u', '--url', help='target url', default="google.com/{0}{1}")
@@ -120,13 +163,16 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--data', help='POST Data', default='')
     parser.add_argument('-X', '--method', help='http Method', default='GET')
     parser.add_argument('-sf','--statusCode', help='http status code filter ex) 200,404', default='200,204,301,302,307,401,404,403,405,500')
-    parser.add_argument('--timeout', type=float, help='requests time out', default=0.1)
+    parser.add_argument('--timeout', help='requests time out', default = '1')
     parser.add_argument('-f', '--flag', help='check it is included', default='')
     
+    parser.add_argument('-t', '--thread', type=int, help='max thread count', default=100)
+    
     args = parser.parse_args()
-
+    
     fuzz = Fuzz(args)
     fuzz.run()
     for thread in fuzz.threads:
         thread.join()
-    os.system("pause")
+    fuzz.exit()
+    # os.system("pause")
